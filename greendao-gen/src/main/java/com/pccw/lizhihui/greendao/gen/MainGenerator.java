0b7121b54372bc935126681248e60296dccf550e
lizhihui0215 <lizhihui0215@gmail.com>
2016-04-15 10:21:57 +0800

    fix bug

---
+++ greendao-gen/src/main/java/com/pccw/lizhihui/greendao/gen/MainGenerator.java
@@ -0,0 +1,56 @@
+package com.pccw.lizhihui.greendao.gen;
+
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Property;
+import de.greenrobot.daogenerator.Schema;
+
+public class MainGenerator {
+    private static final String PROJECT_DIR = System.getProperty("user.dir");
+
+    public static void main(String[] args) throws Exception{
+        Schema schema = new Schema(1000,"com.pccw.lizhihui.cmcc.data.greendao.db");
+        MainGenerator.addTables(schema);
+
+        DaoGenerator daoGenerator =  new DaoGenerator();
+
+        daoGenerator.generateAll(schema, PROJECT_DIR + "/data/src/main/java");
+
+    }
+
+    private static void addTables(final Schema schema){
+        Entity user = addUser(schema);
+
+        Entity department = addDepartment(schema);
+
+        Property departmentId = user.addLongProperty("departmentId").getProperty();
+
+        department.addToOne(user,departmentId);
+
+        user.addToMany(department, departmentId, "deptList");
+    }
+
+    private static Entity addDepartment(final Schema schema){
+        Entity department = schema.addEntity("DepartmentEntity");
+        department.addIdProperty().primaryKey().autoincrement();
+        department.addStringProperty("deptName");
+        department.addStringProperty("deptCode");
+        department.addStringProperty("parentCode");
+        department.addStringProperty("deptId");
+        return department;
+    }
+
+    private static Entity addUser(Schema schema) {
+        Entity user = schema.addEntity("UserEntity");
+        user.addIdProperty().primaryKey().autoincrement();
+        user.addStringProperty("phone");
+        user.addStringProperty("accessToken");
+        user.addStringProperty("token");
+        user.addStringProperty("userId");
+        user.addStringProperty("userName");
+        user.addStringProperty("account");
+
+        return user;
+    }
+
+}
---
+++ greendao-gen/src/main/java/com/pccw/lizhihui/greendao/gen/Test.java
@@ -0,0 +1,7 @@
+package com.pccw.lizhihui.greendao.gen;
+
+/**
+ * Created by lizhihui on 4/15/16.
+ */
+public class Test {
+}
